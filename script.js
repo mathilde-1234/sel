154039dd3449fa6201e5b5bc0c44cb6ae62746e8
mathilde-1234 <pivette.mathilde@gmail.com>
2025-06-30 18:05:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+  fetch("stats.csv")
+      .then(response => response.text())
+      .then(data => {
+        const lignes = data.trim().split("\n");
+        const table = document.getElementById("csvTable");
 
+        lignes.forEach(ligne => {
+          const row = table.insertRow();
+          const cellules = ligne.split(","); // ou ";" selon le fichier
+
+          cellules.forEach(cellule => {
+            const cell = row.insertCell();
+            cell.textContent = cellule.trim();
+          });
+        });
+      })
+      .catch(error => {
+        console.error("Erreur lors du chargement du fichier CSV :", error);
+      });
