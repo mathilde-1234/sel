409dcd93266e88d9932d7e6ed10d95b6437acc56
mathilde-1234 <pivette.mathilde@gmail.com>
2025-09-17 16:49:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,15 +24,18 @@ async function getSaltPer100g(barcode) {
     console.error('Erreur lors de la récupération des données :', error);
   }
 }
-// On récupère l'élément input et le bouton
+
+// Récupération de l'élément input et du bouton
 const input = document.getElementById('codebar');
 const btn = document.getElementById('btn');
 
-// On ajoute un événement au clic sur le bouton
+// Ajout d'un événement au clic sur le bouton
 btn.addEventListener('click', () => {
-    // On récupère la valeur de l'input
-    const codebar = input.value;
+  // Récupération de la valeur de l'input
+  const codebar = input.value.trim();
+  if (codebar) {
+    getSaltPer100g(codebar);
+  } else {
+    console.log('Veuillez entrer un code-barres valide.');
+  }
 });
-// Exemple d'utilisation
-getSaltPer100g(codebar); // Remplace par le code-barres souhaité
-
